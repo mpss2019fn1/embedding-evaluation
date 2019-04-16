97710d4394f15abb2a6131a8640b7c531da67d4c
Duy Hung Nguyen <duyhung.nguyen@student.hpi.de>
2019-04-16 21:42:10 +0200

    added de-/serialization for diff vectors

--- evaluation.py
+++ evaluation.py
@@ -1,5 +1,6 @@
 import argparse
 import numpy as np
+import os
 from csv import DictReader
 from pathlib import Path
 from gensim_loader import GensimLoader
@@ -27,14 +28,28 @@ def compute_vec_similarity(vectors):
 
 
 def main(args):
-    vec_loader = GensimLoader(args.embedding_file)
-    for query_result_file in args.query_result_directory.iterdir():
-        with query_result_file.open() as f:
-            csv_reader = DictReader(f)
-            vec_diffs = [difference_vector(vec_loader, row['a'].split('/Q')[-1], row['b'].split('/Q')[-1]) for row in
-                         tqdm(csv_reader)]
-        score = compute_vec_similarity(vec_diffs)
-        print(query_result_file.stem, score)
+    if not os.path.exists(args.difference_vector_directory):
+        os.makedirs(args.difference_vector_directory)
+
+    if args.mode == 'api_call' and len(os.listdir(args.difference_vector_directory)) == 0:
+        vec_loader = GensimLoader(args.embedding_file)
+        for query_result_file in args.query_result_directory.iterdir():
+            with query_result_file.open() as f:
+                csv_reader = DictReader(f)
+                vec_diffs = [difference_vector(vec_loader, row['a'].split('/Q')[-1], row['b'].split('/Q')[-1]) for row in
+                             tqdm(csv_reader)]
+            np.save(f'{args.difference_vector_directory}/{query_result_file.stem}', vec_diffs)
+            score = compute_vec_similarity(vec_diffs)
+            print(query_result_file.stem, score)
+    elif args.mode == 'cos_eval':
+        for query_result_file in args.difference_vector_directory.iterdir():
+            vec_diffs = np.load(query_result_file)
+            score = compute_vec_similarity(vec_diffs)
+            print(query_result_file.stem, score)
+    elif args.mode == 'avg_eval':
+        pass
+    else:
+        print("api results already exist or mode not specified")
 
 
 if __name__ == '__main__':
@@ -57,5 +72,13 @@ if __name__ == '__main__':
         help="Path to the embedding file directory",
         default="doc2vec.binary.model",
     )
+    parser.add_argument(
+        '-mode',
+        help='Specify with keywords if api needs to be called. Keywords: [api_call, cos_eval, avg_eval]',
+        type=str,
+        default='api_call',
+    )
     args = parser.parse_args()
+    if args.mode not in ['api_call', 'eval_only']:
+        parser.error('Valid modes are ["api_call", "cos_eval", "avg_eval"].')
     main(args)
