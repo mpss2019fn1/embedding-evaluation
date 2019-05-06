7c2113b199de8df6340141955b279d9a00594718
Alexander Riese <alexander.riese@student.hpi.de>
2019-05-06 14:14:31 +0200

    refactor similarity task

--- tasks/similarity_task.py
+++ tasks/similarity_task.py
@@ -1,19 +1,17 @@
-from .task import Task
-
 from csv import DictReader
-from tqdm import tqdm
+from math import isnan
 
 from scipy.stats import spearmanr
+from tqdm import tqdm
+
+from .task import Task
 
 
 class SimilarityTask(Task):
 
-    def __init__(self, csv_wikidata_results, metric, gensim_loader):
-        super().__init__(csv_wikidata_results, metric, gensim_loader)
-        self.our_similarities = []
-        self.data_similarities = []
-
-    def score(self):
+    def __call__(self, *args, **kwargs):
+        our_similarities = []
+        data_similarities = []
         csv_reader = DictReader(self.csv_wikidata_results)
         header_fields = csv_reader.fieldnames
         assert len(header_fields) == 3
@@ -21,9 +19,8 @@ class SimilarityTask(Task):
         for row in pbar:
             word1 = self.gensim_loader.word_vector(row['word1'])
             word2 = self.gensim_loader.word_vector(row['word2'])
-            self.our_similarities.append(self.metric(word1, word2))
-            self.data_similarities.append(row['score'])
-        return spearmanr(self.our_similarities, self.data_similarities).correlation
-
-    def __call__(self, *args, **kwargs):
-        return self.score()
+            x = self.metric(word1, word2)
+            if not isnan(x):
+                our_similarities.append(x)
+                data_similarities.append(row['score'])
+        return spearmanr(our_similarities, data_similarities).correlation
