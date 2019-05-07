6cf9db4ac90eb9c614d7c3cf8ef175f3284ccfe5
Alexander Riese <alexander.riese@student.hpi.de>
2019-05-07 14:31:06 +0200

    add file task logger

---
+++ file_task_logger.py
@@ -0,0 +1,38 @@
+import os
+from pathlib import Path
+
+import git
+import yaml
+
+
+class NullFileTaskLogger:
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def write_metadata_file(self, *args, **kwargs):
+        pass
+
+    def new_file(self, *args, **kwargs):
+        return Path(os.devnull)
+
+
+class FileTaskLogger:
+    def __init__(self, parent_directory, task):
+        self.task = task
+        self.directory = Path(parent_directory).joinpath(self.task.name)
+        self.directory.mkdir(parents=True)
+        self.write_metadata_file()
+
+    def write_metadata_file(self):
+        repo = git.Repo(search_parent_directories=True)
+        sha = repo.head.object.hexsha
+        metadata = {**self.task.configuration, **{'git-sha': sha}}
+        with self.directory.joinpath('metadata.yaml').open('w') as metadata_file:
+            yaml.dump(metadata, metadata_file)
+
+    def new_file(self, filename):
+        new_file = self.directory.joinpath(filename)
+        new_file.touch()
+        return new_file
+
+
--- tasks/task.py
+++ tasks/task.py
@@ -1,16 +1,17 @@
 from abc import ABC, abstractmethod
-from task_logger import FileTaskLogger, NullFileTaskLogger
+from file_task_logger import FileTaskLogger, NullFileTaskLogger
 
 
 class Task(ABC):
 
-    def __init__(self, name, csv_wikidata_results, metric, gensim_loader):
+    def __init__(self, name, csv_wikidata_results, metric, gensim_loader, logging=False):
         self.name = name
         self.csv_wikidata_results = csv_wikidata_results
         self.metric = metric
         self.gensim_loader = gensim_loader
         self.size = sum(1 for _ in csv_wikidata_results) - 1
         self.csv_wikidata_results.seek(0)
+        self.file_task_logger = FileTaskLogger('logging', self) if logging else NullFileTaskLogger()
 
     @property
     def configuration(self):
