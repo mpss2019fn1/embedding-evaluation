e4bf7c118bd3cf5e7c4e118f341c26518a613722
Alexander Riese <alexander.riese@student.hpi.de>
2019-06-03 16:54:29 +0200

    load embedding with word2vec file

--- gensim_loader.py
+++ gensim_loader.py
@@ -1,6 +1,8 @@
-import numpy as np
-from gensim.models.doc2vec import Doc2Vec
-from wikipedia_props_fetcher import WikipediaPropsFetcher
+from gensim.models.keyedvectors import KeyedVectors
+
+
+def load_embedding_file(embedding_file):
+    return KeyedVectors.load_word2vec_format(embedding_file)
 
 
 class EmbeddingEntryNotFound(Exception):
@@ -9,38 +11,12 @@ class EmbeddingEntryNotFound(Exception):
         self.key = key
 
 
-class PropsFetcherEntryNotFound(Exception):
-    def __init__(self, message, key):
-        super().__init__(message)
-        self.key = key
-
-
 class VectorLoader:
-    def __init__(self, model_file):
-        self.model = Doc2Vec.load(model_file)
-        self.props_fetcher = WikipediaPropsFetcher('data/living_people_wikidata_id_wikipedia_page_id_title.csv', ';')
-        self.null_vector = np.zeros(self.model.vector_size)
+    def __init__(self, embedding_file):
+        self.vectors = load_embedding_file(embedding_file)
 
-    def entity_vector(self, wikidata_id):
-        key = self.props_fetcher.get_identifier(wikidata_id)
-        if key is None:
-            raise PropsFetcherEntryNotFound(f"{key} was not found in the props fetcher file", wikidata_id)
-        return self.get_embedding(key, self.model)
-
-    def word_vector(self, word):
-        return self.get_embedding(word, self.model.wv)
-
-    def get_embedding(self, key, embedding_dictionary):
+    def __getitem__(self, item):
         try:
-            return embedding_dictionary[key]
+            return self.vectors[item]
         except KeyError:
-            raise EmbeddingEntryNotFound(f"{key} was not found in the embedding", key)
-
-    def vectors(self):
-        return self.model.wv.vectors
-
-    @staticmethod
-    def save_to_file(iterable, filename):
-        with open(filename, 'w+') as file:
-            for entry in iterable:
-                file.write(entry + '\n')
+            raise EmbeddingEntryNotFound(f"embedding for key {item} not found", item)
